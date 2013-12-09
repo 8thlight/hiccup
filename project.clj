4f841438a7da4c94f414d6d68a3aadb83a775fe4
Myles Megyesi <mylesmegyesi@me.com>
2013-12-09 14:36:07 -0600

    cleans up profile.clj

--- project.clj
+++ project.clj
@@ -5,13 +5,34 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2080"]]
 
-  :plugins [[codox "0.6.4"]]
+  :jar-exclusions [#"\.cljx"]
+  :source-paths   ["target/generated/src/clj" "src/clj"]
+  :resource-paths ["target/generated/src/cljs"]
+  :test-paths     ["target/generated/test/clj" "test/clj"]
+
+  :profiles {:dev {:plugins [[codox "0.6.4"]]}
+             :cljs {:dependencies [[com.keminglabs/cljx "0.3.1"]
+                                   [lein-cljsbuild "1.0.0"]
+                                   [com.cemerick/clojurescript.test "0.2.1"]]
+                    :plugins [[lein-cljsbuild "1.0.0"]
+                              [com.keminglabs/cljx "0.3.1"]]
+                    :aliases {"test" ["do" "cljsbuild" "clean" "," "cljsbuild" "once"]}}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+
+  :aliases {"cljsbuild" ["with-profile" "cljs" "cljsbuild"]}
 
   :codox {:exclude [hiccup.compiler]
           :src-dir-uri "http://github.com/weavejester/hiccup/blob/1.0.4"
           :src-linenum-anchor-prefix "L"}
 
-  :jar-exclusions [#"\.cljx"]
+  :cljsbuild ~(let [test-command ["bin/cljs-test" "target/unit-test.js"]]
+                {:builds {:dev {:source-paths ["target/generated/src/cljs" "src/cljs" "target/generated/test/cljs" "test/cljs"]
+                                :compiler {:output-to "target/unit-test.js"
+                                           :pretty-print true}
+                                :notify-command test-command}}
+                 :test-commands {"unit" test-command}})
 
   :cljx {:builds [{:source-paths ["src/cljx"]
                    :output-path "target/generated/src/clj"
@@ -24,26 +45,4 @@
                    :rules :clj}
                   {:source-paths ["test/cljx"]
                    :output-path "target/generated/test/cljs"
-                   :rules :cljs}]}
-
-  :source-paths ["target/generated/src/clj" "src/clj"]
-
-  :resource-paths ["target/generated/src/cljs"]
-
-  :test-paths ["target/generated/test/clj" "test/clj"]
-
-  :cljsbuild ~(let [test-command ["bin/cljs-test" "target/unit-test.js"]]
-                {:builds {:dev {:source-paths ["target/generated/src/cljs" "src/cljs" "target/generated/test/cljs" "test/cljs"]
-                                :compiler {:output-to "target/unit-test.js"
-                                           :pretty-print true}
-                                :notify-command test-command}}
-                 :test-commands {"unit" test-command}})
-
-  :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.3.1"]
-                                  [lein-cljsbuild "1.0.0"]
-                                  [com.cemerick/clojurescript.test "0.2.1"]]
-                   :plugins [[lein-cljsbuild "1.0.0"]
-                             [com.keminglabs/cljx "0.3.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+                   :rules :cljs}]})

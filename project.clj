c8e4b1f8abe5cdf1cc10c9eb8e4de6a0fe8eeed3
Myles Megyesi <mylesmegyesi@me.com>
2013-12-09 14:50:13 -0600

    more cleanup on the project.clj

--- project.clj
+++ project.clj
@@ -2,28 +2,33 @@
   :description "A fast library for rendering HTML in Clojure"
   :url "http://github.com/weavejester/hiccup"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2080"]]
-
   :jar-exclusions [#"\.cljx"]
-  :source-paths   ["target/generated/src/clj" "src/clj"]
-  :resource-paths ["target/generated/src/cljs"]
+  :source-paths   ["target/generated/src/clj" "src/clj" ]
+  :resource-paths ["target/generated/src/cljs" "src/cljs"]
   :test-paths     ["target/generated/test/clj" "test/clj"]
 
-  :profiles {:dev {:plugins [[codox "0.6.4"]]}
-             :cljs {:dependencies [[com.keminglabs/cljx "0.3.1"]
+  :prep-tasks ["cljx"]
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]
+                   :plugins [[codox "0.6.4"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+
+             :cljs {:dependencies [[org.clojure/clojure "1.5.1"]
+                                   [org.clojure/clojurescript "0.0-2080"]
+                                   [com.keminglabs/cljx "0.3.1"]
                                    [lein-cljsbuild "1.0.0"]
                                    [com.cemerick/clojurescript.test "0.2.1"]]
                     :plugins [[lein-cljsbuild "1.0.0"]
                               [com.keminglabs/cljx "0.3.1"]]
-                    :aliases {"test" ["do" "cljsbuild" "clean" "," "cljsbuild" "once"]}}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+                    :aliases {"test" ["do" ["cljsbuild" "clean"] ["cljsbuild" "once"]]}}}
 
-  :aliases {"cljsbuild" ["with-profile" "cljs" "cljsbuild"]}
+  :aliases {"cljsbuild" ["with-profile" "cljs" "cljsbuild"]
+            "cljx"      ["with-profile" "cljs" "cljx"]}
 
-  :codox {:exclude [hiccup.compiler]
+  :codox {:exclude [hiccup.compiler hiccup.platform hiccup.abstr]
+          :sources ["target/generated/src/clj" "src/clj"]
           :src-dir-uri "http://github.com/weavejester/hiccup/blob/1.0.4"
           :src-linenum-anchor-prefix "L"}
 
